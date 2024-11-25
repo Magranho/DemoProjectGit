f90745a40a80c94c57832eba7bf5211924af7023
Magnus Holte <magranho@live.no>
2024-11-25 22:01:35 +0100

    yey

--- Second.py
+++ Second.py
@@ -1,16 +1,16 @@
 # Code to open and read a CSV file
 import csv
-def read_cars_file ():
+def read_cars_file():
     # Open the cars .csv file for reading
-    with open ('cars . csv ', mode ='r') as file :
-        csv_reader = csv. reader ( file )
-        next ( csv_reader ) # Skip the header row
+    with open('cars.csv', mode ='r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader) # Skip the header row
         # Read and print each row in the CSV
-        for row in csv_reader :
-            print (row)
+        for row in csv_reader:
+            print(row)
 
 # Run the function to read the file
-read_cars_file ()
+read_cars_file()
 
 #comment
 
